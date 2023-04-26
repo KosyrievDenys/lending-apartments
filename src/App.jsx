baf5fb40a38d81a66221fe2fb98382850f7ada45
denys_kosyriev <utross2241@gmail.com>
2023-04-26 12:48:32 +0300

    fix-swiper-and-all

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
 //libs
 import cn from 'classnames';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 //assets
 import styles from './App.module.scss';
@@ -20,8 +20,18 @@ import room1 from './assets/images/room1.jpg';
 import room2 from './assets/images/room2.jpg';
 import room3 from './assets/images/room3.jpg';
 import adaptive from './assets/images/adaptive.png';
+import 'swiper/swiper.css';
 
 function App() {
+
+  const photos = [
+
+    { img: room1 },
+    { img: adaptive },
+    { img: room2 },
+    { img: room3 },
+
+  ]
 
   const apartments = [
     {
@@ -48,140 +58,136 @@ function App() {
   ]
 
   return (
-    <main className={styles.app}>
-      <section className={styles.main}>
-        <div className={cn(styles.container, 'container')}>
-          <div className={cn(styles.wrapper, 'wrapper')}>
-            <div className={styles.left}>
-              <img src={logo} alt='' className={styles.logo} />
-              <h3>Квартиры от собственника</h3>
-              <ul>
-                <li>Главная</li>
-                <li>Расположение</li>
-                <li>Квартиры</li>
-                <li>Кто <br></br> собственник</li>
-                <li>Контакты</li>
-              </ul>
-              <div className={styles.buttons}>
-                <a href='#' className={cn('btn color-red', styles.btn)}>Выбрать квартиру</a>
-                <div className={cn('socials', styles.socials)}>
-                  <a href='#'><img src={viber} alt='' className='icon' /></a>
-                  <a href='#'><img src={facebook} alt='' className='icon' /></a>
-                </div>
-              </div>
-            </div>
-            <div className={styles.right}>
-              <img src={mainImg} alt='' className='border-radius-30px' />
-              <h2 className='color-blue'>Квартиры с ремонтом от собственника</h2>
-              <h1><span className='color-red'>0%</span>Без комисий</h1>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/*<section className={styles.photos}>*/}
-      {/*<div className={cn(styles.container, 'container')}>*/}
-      {/*  <h2>Фото квартир с ремонтом</h2>*/}
-      {/*  <div className='swiper swiper-rooms'>*/}
-      {/*    <div className='swiper-wrapper'>*/}
-      {/*      <div className='swiper-slide'>*/}
-      {/*        <img src={room1} alt='' className='border-radius-30px' />*/}
-      {/*      </div>*/}
-      {/*      <div className='swiper-slide'>*/}
-      {/*        <img src={room2} alt='' className='border-radius-30px' />*/}
-      {/*      </div>*/}
-      {/*      <div className='swiper-slide'>*/}
-      {/*        <img src={room3} alt='' className='border-radius-30px' />*/}
-      {/*      </div>*/}
-      {/*      <div className='swiper-slide'>*/}
-      {/*        <img src={mainImg} alt='' className='border-radius-30px' />*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*    <div className="swiper-btn swiper-button-prev"></div>*/}
-      {/*    <div className="swiper-btn swiper-button-next"></div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-      {/*</section>*/}
-      {apartments.map((apartment, index) => (
-        <section className={styles.apartments}>
+    <>
+      <main className={styles.app}>
+        <section className={styles.main}>
           <div className={cn(styles.container, 'container')}>
             <div className={cn(styles.wrapper, 'wrapper')}>
-              <div className={styles.apartment} key={index}>
-                <div className={styles.apartmentSelection}>
-                  <p className={styles.numRooms}>{apartment.numRooms}</p>
-                  <h2 className='color-blue'>{apartment.type}</h2>
-                  <p className={styles.description}>{apartment.description}</p>
-                  <a href='#' className='color-red btn'>Выбрать квартиру</a>
-                </div>
-                <div className={styles.scheme}>
-                  <img src={apartment.scheme} alt='' />
-                  <div className={styles.square}>
-                    <span>{apartment.square}</span>
-                    Площадь, м2
+              <div className={styles.left}>
+                <img src={logo} alt='' className={styles.logo} />
+                <h3>Квартиры от собственника</h3>
+                <ul>
+                  <li>Главная</li>
+                  <li>Расположение</li>
+                  <li>Квартиры</li>
+                  <li>Кто <br></br> собственник</li>
+                  <li>Контакты</li>
+                </ul>
+                <div className={styles.buttons}>
+                  <a href='#' className={cn('btn color-red', styles.btn)}>Выбрать квартиру</a>
+                  <div className={cn('socials', styles.socials)}>
+                    <a href='#'><img src={viber} alt='' className='icon' /></a>
+                    <a href='#'><img src={facebook} alt='' className='icon' /></a>
                   </div>
                 </div>
+              </div>
+              <div className={styles.right}>
+                <img src={mainImg} alt='' className='border-radius-30px' />
+                <h2 className='color-blue'>Квартиры с ремонтом от собственника</h2>
+                <h1><span className='color-red'>0%</span>Без комисий</h1>
               </div>
             </div>
           </div>
         </section>
-      ))}
-      <section className={styles.location}>
-        <div className={cn(styles.container, 'container')}>
-          <h2>
-            Все квартиры находятся в Харькове в <span>компексе ЖК Левада-2</span> от застройщика АО»Трест Жилстрой-1»
-          </h2>
-          <div className={styles.content}>
-            <div className={styles.description}>
-              <p>
-                Расположение очень удобное: 5 минут до метро проспект Гагарина, 15 минут до центра города пешком
-              </p>
-              <p>
-                Рядом автовокзал и железнодорожная станция Левада
-              </p>
-              <p>
-                Комплекс имеет большую придомовую территорию. Охраняемый многоуровневый паркинг рядом.
-              </p>
-              <p>Рядом школы, детские сады.</p>
-              <p>
-                В комплексе предусмотрены детские площадки, зоны для отдыха и занятий спортом.
-              </p>
-              <p>
-                Весь комплекс состоит из двенадцати 16-ти этажных домов комфорт класса
-              </p>
-            </div>
-            <img src={buildings1} alt='' className={styles.buildings} />
-            <img src={building1} alt='' className={cn(styles.building, 'border-radius-50')} />
+        <section className={styles.photos}>
+          <div className={cn(styles.container, 'container')}>
+            <h2>Фото квартир с ремонтом</h2>
+            <Swiper
+              slidesPerView={'auto'}
+              centeredSlides={true}
+              grabCursor={true}
+              spaceBetween={10}
+            >
+              {photos.map((photo, index) => (
+                <SwiperSlide key={index}>
+                  <img src={photo.img} alt='' className='border-radius-30px' />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
-        </div>
-      </section>
-      <section className={styles.host}>
-        <div className={cn(styles.container, 'container')}>
-          <div className={cn(styles.wrapper, 'wrapper')}>
-            <div className={styles.images}>
-              <img src={host} alt='' className={styles.photo} />
-              <img src={room2} alt='' className={cn(styles.room, 'border-radius-50')} />
+        </section>
+        {apartments.map((apartment, index) => (
+          <section className={styles.apartments}>
+            <div className={cn(styles.container, 'container')}>
+              <div className={cn(styles.wrapper, 'wrapper')}>
+                <div className={styles.apartment} key={index}>
+                  <div className={styles.apartmentSelection}>
+                    <p className={styles.numRooms}>{apartment.numRooms}</p>
+                    <h2 className='color-blue'>{apartment.type}</h2>
+                    <p className={styles.description}>{apartment.description}</p>
+                    <a href='#' className='color-red btn'>Выбрать квартиру</a>
+                  </div>
+                  <div className={styles.scheme}>
+                    <img src={apartment.scheme} alt='' />
+                    <div className={styles.square}>
+                      <span>{apartment.square}</span>
+                      Площадь, м2
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className={styles.description}>
-              <h2>Добрый день! <span className='color-blue'>Меня зовут Тимур</span> и я продаю только свои собственные
-                квартиры.</h2>
-              <div className={styles.description}></div>
-              <p>
-                В каждой из квартир я делаю дизайнерский ремонт. Сам лично контролирую все этапы работ, закупаю
-                материалы
-                и
-                мебель.
-              </p>
-              <p>
-                В моих квартирах все очень удобно и адаптированно к жизни (фото в группе все настоящие, именно так и
-                выглядят мои квартиры в жизни)
-              </p>
-              <p>
-                Я подбираю лучшие квартиры у застройщика и отвечаю за качество ремонта.
-              </p>
-              <a href='tel:+380935370000' className='btn'>Позвонить</a>
+          </section>
+        ))}
+        <section className={styles.location}>
+          <div className={cn(styles.container, 'container')}>
+            <h2>
+              Все квартиры находятся в Харькове в <span>компексе ЖК Левада-2</span> от застройщика АО»Трест Жилстрой-1»
+            </h2>
+            <div className={styles.content}>
+              <div className={styles.description}>
+                <p>
+                  Расположение очень удобное: 5 минут до метро проспект Гагарина, 15 минут до центра города пешком
+                </p>
+                <p>
+                  Рядом автовокзал и железнодорожная станция Левада
+                </p>
+                <p>
+                  Комплекс имеет большую придомовую территорию. Охраняемый многоуровневый паркинг рядом.
+                </p>
+                <p>Рядом школы, детские сады.</p>
+                <p>
+                  В комплексе предусмотрены детские площадки, зоны для отдыха и занятий спортом.
+                </p>
+                <p>
+                  Весь комплекс состоит из двенадцати 16-ти этажных домов комфорт класса
+                </p>
+              </div>
+              <img src={buildings1} alt='' className={styles.buildings} />
+              <img src={building1} alt='' className={cn(styles.building, 'border-radius-50')} />
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+        <section className={styles.host}>
+          <div className={cn(styles.container, 'container')}>
+            <div className={cn(styles.wrapper, 'wrapper')}>
+              <div className={styles.images}>
+                <img src={host} alt='' className={styles.photo} />
+                <img src={room2} alt='' className={cn(styles.room, 'border-radius-50')} />
+              </div>
+              <div className={styles.description}>
+                <h2>Добрый день! <span className='color-blue'>Меня зовут Тимур</span> и я продаю только свои собственные
+                  квартиры.</h2>
+                <div className={styles.description}></div>
+                <p>
+                  В каждой из квартир я делаю дизайнерский ремонт. Сам лично контролирую все этапы работ, закупаю
+                  материалы
+                  и
+                  мебель.
+                </p>
+                <p>
+                  В моих квартирах все очень удобно и адаптированно к жизни (фото в группе все настоящие, именно так и
+                  выглядят мои квартиры в жизни)
+                </p>
+                <p>
+                  Я подбираю лучшие квартиры у застройщика и отвечаю за качество ремонта.
+                </p>
+                <a href='tel:+380935370000' className='btn'>Позвонить</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
       <footer className={styles.footer}>
         <div className={cn(styles.container, 'container')}>
           <div className={cn(styles.wrapper, 'wrapper')}>
@@ -207,9 +213,9 @@ function App() {
           </div>
         </div>
       </footer>
-      <script src="./assets/libs/swiper/swiper.js"></script>
-    </main>
-  );
+    </>
+  )
+    ;
 }
 
 export default App;
